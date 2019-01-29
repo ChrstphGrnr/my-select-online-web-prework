c6d8c1207624364cd869315aafb59174b191d012
Christoph Gruener <christophgruener@me.com>
2019-01-29 23:15:00 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  i = 0 
+ selectors = []
+ while i < collection.length 
+  if yield collection[i] == true
+    selectors.push(collection[i])
+  else 
+  end
+ i += 1 
+end 
+selectors
 end
