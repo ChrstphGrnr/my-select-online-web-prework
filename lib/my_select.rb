c0a42735f4c5c25f89b783249f05b4f81fcc32a4
Christoph Gruener <christophgruener@me.com>
2019-01-29 23:23:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  i = 0 
  selectors = []
  while i < collection.length 
-  if (yield collection[i] == true)
+  if yield collection[i] == true
     selectors.push(collection[i])
   end
  i += 1 
